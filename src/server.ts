77de5be36564a01dd29180e012c3d9b12e432518
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-09-18 15:41:43 -0300

    chore: deploy config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import { app } from "@/app";
+import { env } from "./env";
 
-const PORT = 3333
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(env.PORT, () => console.log(`Server is running on port ${env.PORT}`))
